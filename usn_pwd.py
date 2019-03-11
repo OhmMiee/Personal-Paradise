ee5ed06bd716dec6421da82ac404827f1e3421b7
OhmMiee <48233965+OhmMiee@users.noreply.github.com>
2019-03-11 09:33:01 +0700

    Add files via upload

---
+++ usn_pwd.py
@@ -0,0 +1,19 @@
+def MENU():
+    kfjhfdfheof
+
+def HOME():
+    jdfgjj
+    fhj
+    sdjkdj
+    MENU()
+
+username = 'uSer'
+password = 'pAss'
+
+usn = input('Please enter your Username: ')
+if usn == username:
+    pwd = input('Please enter your Password: '):
+        if pwd == password:
+            HOME()
+        else :
+            print ("Username or password is wrong")
